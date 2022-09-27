ec14a10225be2d55ccfe853e978f348447eebfcf
Laura Abbott <laura@oxide.computer>
2022-09-27 16:54:47 -0400

    Move areas.rs to a dedicated crate
    This allows those regions to be used directly

--- lpc55_sign/src/sign_ecc.rs
+++ lpc55_sign/src/sign_ecc.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::areas::*;
+use lpc55_areas::*;
 use anyhow::Result;
 use byteorder::{ByteOrder, WriteBytesExt};
 use elliptic_curve::generic_array::typenum::Unsigned;
